7a4e464d5404d9a039f620e56184f6904b38a04a
AlexanderWindecker <waa_alex@hotmail.com>
2024-02-29 15:10:07 -0300

    Chuck Norris API testing

---
+++ cypress/e2e/Tests/apiChuckNorris.cy.js
@@ -0,0 +1,21 @@
+describe("Chuck Norris API Testing", () => {
+  
+  it("Validar crear 5 chistes aleatorios de Chuck Norris", () => {
+    const creaChiste = new Set();
+
+    for (let i = 0; i < 5; i++) {
+      cy.request("https://api.chucknorris.io/jokes/random")
+        .then((response) => {
+          expect(response.status).to.eq(200);
+          expect(response.body).to.have.property("value").that.is.a("string");
+
+          const broma = response.body.value;
+
+          expect(creaChiste.has(broma)).to.be.false;
+          creaChiste.add(broma);
+
+          cy.log(`Chiste ${i + 1}: ${broma}`);
+        });
+    }
+  });
+});
